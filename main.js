acd8a2962264d04dcb14ff72eb31ac0e79f0cfd1
patrick2003 <patrick.gloss@web.de>
2019-06-15 22:45:54 +0200

    fertig

---
+++ main.js
@@ -0,0 +1,45 @@
+const config = require('./config.json');
+let workshops = require('./workshops.json');
+const api = require('./api.js');
+const messages = require('./messages.js');
+const Telegraf = require('telegraf');
+const { Markup } = Telegraf;
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+
+const bot = new Telegraf(config.botToken);
+
+bot.command('overview', (ctx) => ctx.reply(messages.overview(workshops.data)));
+bot.command('info', (ctx) => ctx.reply(messages.dates(workshops.data), messages.createKeyboard(workshops.data).extra()));
+bot.launch();
+
+
+bot.action(/info_(.*)/, (ctx) => {
+  ctx.reply(messages.createInfo(ctx.match[1], workshops.data), { parse_mode: 'HTML' });
+});
+
+
+let requestedData = {};
+
+setInterval(() => {
+  if (Object.keys(requestedData).length === requestedData.size + 1) {
+    workshops.data = requestedData;
+    requestedData = {};
+    console.log("data up to date");
+  } else {
+    api.getWorkshops(0, 1).then((json) => {
+      if (json) {
+        requestedData.size = json.totalItems;
+        if (json.pageCount !== 0) {
+          for (let page = 0; page < json.pageCount; page++) {
+            api.getWorkshops(page, 1).then(
+              (json) => {
+                json.data.forEach((element) => requestedData[element.id] = element);
+              })
+          }
+        }
+      }
+    });
+  }
+}, 5000);
--- messages.js
+++ messages.js
@@ -1,9 +1,13 @@
+const Telegraf = require('telegraf');
+const { Markup } = Telegraf;
+
 const overview = (data) => {
-let message = ''; 
+  let message = '';
   if (data[0] > 0) {
     message = 'Es gibt folgende Workshops: \n';
     data.forEach(element => {
       if (element.topic) {
+        message += '> '
         message += element.topic;
         message += '\n';
       }
@@ -14,4 +18,50 @@ let message = '';
   return message;
 };
 
-module.exports = {overview};
+const dates = (data) => {
+  let message = '';
+  if (data.size > 0) {
+    message = 'Wähle den Workshop zu dem du mögliche Termine erhalten willst';
+  } else {
+    message = 'Es sind zur Zeit keine Workshops eingetragen';
+  }
+
+  return message;
+};
+
+
+const createButtons = (data) => {
+  const buttons = [];
+
+  if (data.size > 0) {
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        const element = data[key];
+        if (element.topic) {
+          buttons.push([Markup.callbackButton(element.topic, ('info_' + element.id))]);
+        }
+      }
+    }
+  }
+
+  return buttons;
+};
+
+const createKeyboard = (data) => {
+  const keyboard = Markup.inlineKeyboard(createButtons(data));
+
+  return keyboard;
+}
+
+const createInfo = (match, data) => {
+  let answer = '';
+  if (data.size > 0) {
+    answer = '<b>' + data[match].topic + '</b> \n';
+    answer += data[match].description;
+  } else {
+    answer = 'Es liegen keine Daten vor';
+  }
+  return answer;
+}
+
+module.exports = { overview, dates, createKeyboard, createInfo };
